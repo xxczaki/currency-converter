d96049493a78df1726eec82713e13e7ce6bba575
Antoni Kepinski <xxczaki@pm.me>
2019-04-09 00:27:20 +0200

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -36,8 +36,8 @@ export default class MyDocument extends Document {
 					<link rel="manifest" href="static/manifest.json"/>
 					<link rel="icon" href="static/favicon.png"/>
 					<link rel="apple-touch-icon" href="static/apple-touch-icon.png"/>
-					<link href="static/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-					<link href="static/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+					<link href="static/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>
+					<link href="static/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>
 					<link href="static/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>
 					<link href="static/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image"/>
 					<link href="static/splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image"/>
