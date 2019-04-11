bd8812c1140826a94336ba38f900d158fba3e84d
Antoni Kepinski <xxczaki@pm.me>
2019-04-11 21:07:35 +0200

    Make color contrast better, according to Lighthouse report

--- components/convert-button.js
+++ components/convert-button.js
@@ -8,18 +8,18 @@ const Button = styled.button`
     font-size: 16px;
     cursor: pointer;
     color: #fff;
-    background-color: #689F38;
+    background-color: #8BC34A;
     outline: none;
     user-select: none;
     background-position: center;
     transition: background 0.8s;
 
     &:hover {
-        background: #558B2F radial-gradient(circle, transparent 1%, #558B2F 1%) center/15000%;
+        background: #7CB342 radial-gradient(circle, transparent 1%, #7CB342 1%) center/15000%;
     }
 
     &:active {
-        background-color: #33691E;
+        background-color: #689F38;
         background-size: 100%;
         transition: background 0s;
     }
--- components/reset-button.js
+++ components/reset-button.js
@@ -9,18 +9,18 @@ const Reset = styled.button`
     font-size: 16px;
     cursor: pointer;
     color: #fff;
-    background-color: #f44336;
+    background-color: #ef5350;
     outline: none;
     user-select: none;
     background-position: center;
     transition: background 0.8s;
 
     &:hover {
-        background: #e53935 radial-gradient(circle, transparent 1%, #e53935 1%) center/15000%;
+        background: #f44336 radial-gradient(circle, transparent 1%, #f44336 1%) center/15000%;
     }
 
     &:active {
-        background-color: #d32f2f;
+        background-color: #e53935;
         background-size: 100%;
         transition: background 0s;
     }
