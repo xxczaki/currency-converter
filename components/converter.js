d37225bf49f40bf1730e68f1d1e0b9922a4ade0c
Antoni Kepinski <xxczaki@pm.me>
2019-04-11 21:08:05 +0200

    Reset the state as well, when the reset button is pressed

--- components/converter.js
+++ components/converter.js
@@ -27,6 +27,8 @@ const ConverterSchema = Yup.object().shape({
 const Converter = () => {
 	const [result, setResult] = useState('');
 
+	const resetState = () => setResult('');
+
 	return (
 		<div>
 			<Formik
@@ -44,7 +46,7 @@ const Converter = () => {
 						if (data === null) {
 							const request = await fetch(`https://api.exchangeratesapi.io/latest?base=${values.from}`);
 							const response = await request.json();
-							LocalStorage.put('exchangeRates', response);
+							await LocalStorage.put('exchangeRates', response);
 
 							money.base = response.base;
 							money.rates = response.rates;
@@ -74,8 +76,11 @@ const Converter = () => {
 						<Button type="submit" disabled={isSubmitting}>
 							Convert
 						</Button>
-						<Reset type="reset" onClick={() => resetForm()}>
-							X
+						<Reset type="reset" onClick={() => {
+							resetForm();
+							resetState();
+						}}
+						>X
 						</Reset>
 						<br/>
 						<br/>
