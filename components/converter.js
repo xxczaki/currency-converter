965ba06291b9e4545194f719b0516ffd2fdd8a5a
Antoni Kepinski <xxczaki@pm.me>
2019-04-14 12:03:33 +0200

    Use localforage instead of ttl-localstorage for better performance :rocket:

--- components/converter.js
+++ components/converter.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Formik, Form} from 'formik';
-import {LocalStorage} from 'ttl-localstorage';
+import localforage from 'localforage';
 import money from 'money';
 import * as Yup from 'yup';
 
@@ -9,9 +9,6 @@ import From from './from';
 import To from './to';
 import Button from './convert-button';
 import Reset from './reset-button';
-
-// Cache API results for 1 hour
-LocalStorage.timeoutInSeconds = 3600;
 
 // Validation
 const ConverterSchema = Yup.object().shape({
@@ -38,15 +35,18 @@ const Converter = () => {
 					to: ''
 				}}
 				validationSchema={ConverterSchema}
-				onSubmit={async (values, {setSubmitting}) => {
+				onSubmit={(values, {setSubmitting}) => {
 					setSubmitting(false);
 
-					LocalStorage.get('exchangeRates').then(async data => {
-						// If there is no cached data, make a request.
-						if (data === null) {
+					localforage.getItem('exchangeRates', async (error, value) => {
+						if (value === null || new Date().toISOString().slice(0, 10) !== value.date) {
 							const request = await fetch(`https://api.exchangeratesapi.io/latest?base=${values.from}`);
 							const response = await request.json();
-							await LocalStorage.put('exchangeRates', response);
+							await localforage.setItem('exchangeRates', response, error => {
+								if (error) {
+									console.log(error);
+								}
+							});
 
 							money.base = response.base;
 							money.rates = response.rates;
@@ -55,8 +55,8 @@ const Converter = () => {
 
 							setResult(`${values.amount} ${values.from} => ${result} ${values.to}`);
 						} else {
-							money.base = data.base;
-							money.rates = data.rates;
+							money.base = value.base;
+							money.rates = value.rates;
 
 							const result = money.convert(values.amount, {from: values.from, to: values.to}).toFixed(3);
 
