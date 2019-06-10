40773860837281e64aff72a8882c6f0cd8083c20
Antoni Kepinski <xxczaki@pm.me>
2019-06-10 17:38:41 +0200

    fix global style not loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import App, {Container} from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
 
 import '../node_modules/modern-normalize/modern-normalize.css';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: #212121;
+        font-family: monospace;
+        margin: auto;
+        width: 80%;
+        font-size: 16px;
+        padding-top: 50px;
+        padding-bottom: 100px;
+        color: #fff;
+        line-height: 18pt;
+        -webkit-font-smoothing: antialiased;
+        -webkit-touch-callout: none;
+        text-rendering: optimizeSpeed;
+    }
+`;
 
 class MyApp extends App {
 	static async getInitialProps({Component, ctx}) {
@@ -20,6 +38,7 @@ class MyApp extends App {
 
 		return (
 			<Container>
+				<GlobalStyle/>
 				<Head>
 					<title>Currency Converter</title>
 				</Head>
