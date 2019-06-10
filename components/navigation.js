e12c174944e7dd75096a4d2ced7b119a48526075
Antoni Kepinski <xxczaki@pm.me>
2019-06-10 17:38:29 +0200

    Add scroll=false to navigation

--- components/navigation.js
+++ components/navigation.js
@@ -3,6 +3,6 @@ import Link from 'next/link';
 
 import ExternalLink from './link';
 
-const Navigation = () => <p style={{margin: '2px'}}><Link prefetch href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link prefetch href="/about"><ExternalLink>[About]</ExternalLink></Link></p>;
+const Navigation = () => <p style={{margin: '2px'}}><Link prefetch scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link prefetch scroll={false} href="/about"><ExternalLink>[About]</ExternalLink></Link></p>;
 
 export default Navigation;
