8185dd1d452e84938208e11b25c4a6ae40938c97
Antoni Kepinski <xxczaki@pm.me>
2019-08-28 19:11:44 +0200

    Remove deprecated Next.js Link prefetch prop

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,6 +4,6 @@ import Link from 'next/link';
 // @ts-ignore
 import ExternalLink from './link';
 
-const Navigation = (): JSX.Element => <p style={{margin: '2px'}}><Link prefetch scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link prefetch scroll={false} href="/about"><ExternalLink>[About]</ExternalLink></Link></p>;
+const Navigation = (): JSX.Element => <p style={{margin: '2px'}}><Link scroll={false} href="/"><ExternalLink>[Home]</ExternalLink></Link> &bull; <Link scroll={false} href="/about"><ExternalLink>[About]</ExternalLink></Link></p>;
 
 export default Navigation;
