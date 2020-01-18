6c8fead40280d10dfaa16f2a2df12ccf2b0d6475
Antoni Kepinski <xxczaki@pm.me>
2020-01-18 15:59:13 +0100

    group currencies based on continent etc.

--- components/converter.js
+++ components/converter.js
@@ -12,40 +12,63 @@ import Select from './select';
 import ButtonGroup from './button-group';
 import Button from './button';
 
-const options = [
-	{value: 'AUD', label: '🇦🇺 Australian dollar'},
-	{value: 'BRL', label: '🇧🇷 Brazilian real'},
-	{value: 'BGN', label: '🇧🇬 Bulgarian lev'},
-	{value: 'CAD', label: '🇨🇦 Canadian dollar'},
-	{value: 'CNY', label: '🇨🇳 Chinese yuan'},
-	{value: 'HRK', label: '🇭🇷 Croatian kuna'},
-	{value: 'CZK', label: '🇨🇿 Czech koruna'},
-	{value: 'DKK', label: '🇩🇰 Danish krone'},
-	{value: 'EUR', label: '🇪🇺 Euro'},
-	{value: 'USD', label: '🇺🇸 US dollar'},
-	{value: 'HKD', label: '🇭🇰 Hong Kong dollar'},
-	{value: 'HUF', label: '🇭🇺 Hungarian forint'},
-	{value: 'ISK', label: '🇮🇸 Icelandic krona'},
-	{value: 'INR', label: '🇮🇳 Indian rupee'},
-	{value: 'IDR', label: '🇮🇩 Indonesian rupiah'},
-	{value: 'ILS', label: '🇮🇱 Israeli shekel'},
-	{value: 'JPY', label: '🇯🇵 Japanese yen'},
-	{value: 'NOK', label: '🇳🇴 Norwegian krone'},
-	{value: 'PLN', label: '🇵🇱 Polish zloty'},
-	{value: 'GBP', label: '🇬🇧 Pound sterling'},
-	{value: 'RON', label: '🇷🇴 Romanian leu'},
-	{value: 'RUB', label: '🇷🇺 Russian rouble'},
-	{value: 'KRW', label: '🇰🇷 South Korean won'},
-	{value: 'MXN', label: '🇲🇽 Mexican peso'},
-	{value: 'MYR', label: '🇲🇾 Malaysian ringgit'},
-	{value: 'NZD', label: '🇳🇿 New Zealand dollar'},
-	{value: 'PHP', label: '🇵🇭 Philippine peso'},
-	{value: 'SGD', label: '🇸🇬 Singapore dollar'},
-	{value: 'THB', label: '🇹🇭 Thai baht'},
-	{value: 'ZAR', label: '🇿🇦 South African rand'},
-	{value: 'SEK', label: '🇸🇪 Swedish krona'},
-	{value: 'CHF', label: '🇨🇭 Swiss franc'},
-	{value: 'TRY', label: '🇹🇷 Turkish lira'}
+const majors = [
+	{value: 'USD', label: 'US Dollar (USD)'},
+	{value: 'GBP', label: 'UK Pound (GBP)'},
+	{value: 'EUR', label: 'Euro (EUR)'},
+	{value: 'AUD', label: 'Australian Dollar (AUD)'},
+	{value: 'CAD', label: 'Canadian Dollar (CAD)'},
+	{value: 'CNY', label: 'Chinese Yuan Renminbi (CNY)'},
+	{value: 'JPY', label: 'Japanese Yen (JPY)'},
+	{value: 'CHF', label: 'Swiss Franc (CHF)'}
+];
+
+const europe = [
+	{value: 'BGN', label: 'Bulgarian Lev (BGN)'},
+	{value: 'HRK', label: 'Croatian Kuna (HRK)'},
+	{value: 'CZK', label: 'Czech Koruna (CZK)'},
+	{value: 'DKK', label: 'Danish Krone (DKK)'},
+	{value: 'EUR', label: 'Euro (EUR)'},
+	{value: 'HUF', label: 'Hungarian Forint (HUF)'},
+	{value: 'ISK', label: 'Icelandic Krona (ISK)'},
+	{value: 'NOK', label: 'Norwegian Krone (NOK)'},
+	{value: 'PLN', label: 'Polish New Zloty (PLN)'},
+	{value: 'RON', label: 'Romanian Leu (RON)'},
+	{value: 'RUB', label: 'Russian Ruble (RUB)'},
+	{value: 'SEK', label: 'Swedish Krona (SEK)'},
+	{value: 'CHF', label: 'Swiss Franc (CHF)'},
+	{value: 'TRY', label: 'Turkish Lira (TRY)'},
+	{value: 'GBP', label: 'UK Pound (GBP)'}
+];
+
+const americas = [
+	{value: 'BRL', label: 'Brazilian Real (BRL)'},
+	{value: 'CAD', label: 'Canadian Dollar (CAD)'},
+	{value: 'MXN', label: 'Mexican New Peso (MXN)'},
+	{value: 'USD', label: 'US Dollar (USD)'}
+];
+
+const africa = [
+	{value: 'ZAR', label: 'South African Rand (ZAR)'}
+];
+
+const asia = [
+	{value: 'CNY', label: 'Chinese Yuan Renminbi (CNY)'},
+	{value: 'HKD', label: 'Hong Kong Dollar (HKD)'},
+	{value: 'INR', label: 'Indian Rupee (INR)'},
+	{value: 'IDR', label: 'Indonesian Rupiah (IDR)'},
+	{value: 'ILS', label: 'Israeli Shekel (ILS)'},
+	{value: 'JPY', label: 'Japanese Yen (JPY)'},
+	{value: 'MYR', label: 'Malaysian Ringgit (MYR)'},
+	{value: 'PHP', label: 'Philippine Peso (PHP)'},
+	{value: 'SGD', label: 'Singapore Dollar (SGD)'},
+	{value: 'KRW', label: 'South Korean Won (KRW)'},
+	{value: 'THB', label: 'Thai Baht (THB)'}
+];
+
+const oceania = [
+	{value: 'AUD', label: 'Australian Dollar (AUD)'},
+	{value: 'NZD', label: 'New Zealand Dollar (NZD)'}
 ];
 
 const Converter = () => {
@@ -65,8 +88,6 @@ const Converter = () => {
 	};
 
 	const onSubmit = async values => {
-		// E.preventDefault();
-
 		const {Cashify} = await import('cashify');
 		const cashify = new Cashify({base: data.base, rates: data.rates});
 		const result = cashify.convert(Number(values.amount), {from: values.from, to: values.to}).toFixed(3);
@@ -90,25 +111,79 @@ const Converter = () => {
         Amount
 					<Input ref={register({required: true})} type="number" min="1" step="any" pattern="[0-9]*" name="amount" placeholder="Amount"/>
 				</Label>
-				<Label>
+				<SelectAddon>
+					<Label>
         From
-					<SelectAddon>
 						<Select ref={register({required: true})} name="from">
 							<option value="">Select</option>
-							{options.map(el => (
-								<option key={el.value} value={el.value}>{el.label}</option>
-							))}
+							<optgroup label="Majors">
+								{majors.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
+							<optgroup label="Europe">
+								{europe.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
+							<optgroup label="Americas">
+								{americas.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
+							<optgroup label="Africa">
+								{africa.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
+							<optgroup label="Asia">
+								{asia.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
+							<optgroup label="Oceania">
+								{oceania.map(el => (
+									<option key={el.value} value={el.value}>{el.label}</option>
+								))}
+							</optgroup>
 						</Select>
-						<Button type="button" onClick={() => swap()}>🔃</Button>
-					</SelectAddon>
-				</Label>
+					</Label>
+					<Button type="button" onClick={() => swap()}>🔃</Button>
+				</SelectAddon>
 				<Label>
         To
 					<Select ref={register({required: true})} name="to">
 						<option value="">Select</option>
-						{options.map(el => (
-							<option key={el.value} value={el.value}>{el.label}</option>
-						))}
+						<optgroup label="Majors">
+							{majors.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
+						<optgroup label="Europe">
+							{europe.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
+						<optgroup label="Americas">
+							{americas.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
+						<optgroup label="Africa">
+							{africa.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
+						<optgroup label="Asia">
+							{asia.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
+						<optgroup label="Oceania">
+							{oceania.map(el => (
+								<option key={el.value} value={el.value}>{el.label}</option>
+							))}
+						</optgroup>
 					</Select>
 				</Label>
 				<ButtonGroup>
